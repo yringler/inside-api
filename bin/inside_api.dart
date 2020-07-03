39fd9223d9048617840b24e23e5c6e7a5fa07f80
Yehuda Ringler <yrappdev@gmail.com>
2020-07-03 08:01:57 -0400

    initial commit

--- bin/inside_api.dart
+++ bin/inside_api.dart
@@ -1,3 +1,21 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+import 'package:flutter_wordpress/flutter_wordpress.dart';
+
+void main(List<String> arguments) async {
+  final wordPress = WordPress(baseUrl: 'http://localhost');
+
+  var posts = await wordPress.fetchPosts(
+    postParams: ParamsPostList(
+      context: WordPressContext.view,
+      pageNum: 1,
+      perPage: 20,
+      order: Order.desc,
+      orderBy: PostOrderBy.date,
+    ),
+  );
+
+  for (final post in posts.getRange(1, 3)) {
+    print(post.title.rendered.trim());
+    print(post.content.rendered.trim());
+    print('\n');
+  }
 }
