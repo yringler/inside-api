06b769756752478adbe1175a9ad919ec7f78acd8
Yehuda Ringler <yrappdev@gmail.com>
2020-07-08 23:43:14 -0400

    counts audio

--- bin/main.dart
+++ bin/main.dart
@@ -4,5 +4,6 @@ import 'package:inside_api/models.dart';
 
 void main(List<String> arguments) async {
   final site = await fromWordPress('http://localhost');
+  site.setAudioCount();
   print(jsonEncode(site));
 }
