c4bacf4ec97fb3fa1181757e6b685189735fdf7a
Yehuda Ringler <yrappdev@gmail.com>
2020-07-22 10:38:45 -0400

    Remove extra site service. We just need boxes of data

--- lib/site-service.dart
+++ lib/site-service.dart
@@ -6,68 +6,40 @@ import 'package:hive/hive.dart';
 import 'package:hive/src/hive_impl.dart';
 import 'package:inside_api/models.dart';
 
-/// Provide access to site data. Stored using HiveDB
-class SiteService {
-  final _SiteBoxes boxes;
+/// [hivePath] is where the data should be stored.
+/// [currentVersion] is the date which any saved data should be newer than or equal to.
+/// [dataSource] is (for now) a file path which contains JSON of the entire site.
+Future<SiteBoxes> getSiteBoxesWithData(
+    {String hivePath, DateTime currentVersion, String dataSource}) async {
+  final boxes = await _getSiteBoxesNoData();
 
-  SiteService({this.boxes});
-
-  /// [hivePath] is where the data should be stored.
-  /// [currentVersion] is the date which any saved data should be newer than or equal to.
-  /// [dataSource] is (for now) a file path which contains JSON of the entire site.
-  static Future<SiteService> create(
-      {String hivePath, DateTime currentVersion, String dataSource}) async {
-    final boxes = await getHive();
-
-    if (boxes.createdDate != null &&
-        (currentVersion == null ||
-            boxes.createdDate.isAfter(currentVersion) ||
-            boxes.createdDate.isAtSameMomentAs(currentVersion))) {
-      return SiteService(boxes: boxes);
-    }
-
-    // We don't have data, or no current data, so load up the boxes
-    // on another isolate.
-
-    await boxes.hive.close();
-
-    final completer = Completer();
-    final recievePort = ReceivePort();
-
-    await Isolate.spawn(
-        setHiveData, [recievePort.sendPort, hivePath, dataSource]);
-
-    recievePort.listen((_) => completer.complete());
-
-    await completer.future;
-
-    // Returned re-opened boxes.
-    return SiteService(boxes: await getHive(path: hivePath));
+  if (boxes.createdDate != null &&
+      (currentVersion == null ||
+          boxes.createdDate.isAfter(currentVersion) ||
+          boxes.createdDate.isAtSameMomentAs(currentVersion))) {
+    return boxes;
   }
-}
 
-/// Loads data into hive.
-void setHiveData(dynamic arguments) async {
-  final dynamicArguments = List.castFrom(arguments);
-  final sendPort = dynamicArguments[0] as SendPort;
-  final hivePath = dynamicArguments[1] as String;
-  final dataSource = dynamicArguments[2] as String;
-
-  final site = Site.fromJson(json.decode(File(dataSource).readAsStringSync()));
-  final boxes = await getHive(path: hivePath);
-
-  await boxes.sections.putAll(site.sections);
-  await boxes.topItems.putAll(
-      Map.fromEntries(site.topItems.map((e) => MapEntry(e.sectionId, e))));
-  await boxes
-      .setCreatedDate(DateTime.fromMillisecondsSinceEpoch(site.createdDate));
+  // We don't have data, or no current data, so load up the boxes
+  // on another isolate.
 
   await boxes.hive.close();
 
-  sendPort.send(true);
+  final completer = Completer();
+  final recievePort = ReceivePort();
+
+  await Isolate.spawn(
+      _setHiveData, [recievePort.sendPort, hivePath, dataSource]);
+
+  recievePort.listen((_) => completer.complete());
+
+  await completer.future;
+
+  // Returned re-opened boxes.
+  return await _getSiteBoxesNoData(path: hivePath);
 }
 
-class _SiteBoxes {
+class SiteBoxes {
   final HiveImpl hive;
   final LazyBox<Section> sections;
   final Box<TopItem> topItems;
@@ -82,10 +54,32 @@ class _SiteBoxes {
   Future<void> setCreatedDate(DateTime value) async =>
       data.put('key', value.millisecondsSinceEpoch);
 
-  _SiteBoxes({this.hive, this.sections, this.topItems, this.data});
+  SiteBoxes({this.hive, this.sections, this.topItems, this.data});
 }
 
-Future<_SiteBoxes> getHive({String path}) async {
+/// Loads data into hive.
+void _setHiveData(dynamic arguments) async {
+  final dynamicArguments = List.castFrom(arguments);
+  final sendPort = dynamicArguments[0] as SendPort;
+  final hivePath = dynamicArguments[1] as String;
+  final dataSource = dynamicArguments[2] as String;
+
+  final site = Site.fromJson(json.decode(File(dataSource).readAsStringSync()));
+  final boxes = await _getSiteBoxesNoData(path: hivePath);
+
+  await boxes.sections.putAll(site.sections);
+  await boxes.topItems.putAll(
+      Map.fromEntries(site.topItems.map((e) => MapEntry(e.sectionId, e))));
+  await boxes
+      .setCreatedDate(DateTime.fromMillisecondsSinceEpoch(site.createdDate));
+
+  // Close so that can be opened on other isolate.
+  await boxes.hive.close();
+  sendPort.send(true);
+}
+
+/// A smiple open of all the boxes which doesn't check wether they have data or not.
+Future<SiteBoxes> _getSiteBoxesNoData({String path}) async {
   final hive = HiveImpl();
   await Directory(path).create();
   hive.init(path);
@@ -99,7 +93,7 @@ Future<_SiteBoxes> getHive({String path}) async {
     hive.registerAdapter(TopItemAdapter());
   } catch (_) {}
 
-  return _SiteBoxes(
+  return SiteBoxes(
       hive: hive,
       data: await hive.openBox('data'),
       sections: await hive.openLazyBox('sections'),
